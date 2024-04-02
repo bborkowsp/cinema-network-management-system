f6a2a24bf1b03cb42d367970de60f2bfdd88a934
bborkowsp <pawel.borkowski0704@gmail.com>
2024-04-02 15:22:01 +0200

    cinemas list update

---
+++ cinema-frontend/src/app/scaffold-client-side/scaffold-client-side.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-scaffold-client',
+  templateUrl: './scaffold-client.component.html',
+  styleUrls: ['./scaffold-client.component.scss']
+})
+export class ScaffoldClientSideComponent {
+
+}
---
+++ cinema-frontend/src/app/scaffold-client-side/scaffold-client-side.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ScaffoldClientSideComponent} from "./scaffold-client-side.component";
+
+
+@NgModule({
+  declarations: [
+    ScaffoldClientSideComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ScaffoldClientSideModule {
+}
