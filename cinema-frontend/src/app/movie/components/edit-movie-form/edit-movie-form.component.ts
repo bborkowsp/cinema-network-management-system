ba27eb3d82792af0560c8cee2585b99a0cfef14c
bborkowsp <pawel.borkowski0704@gmail.com>
2024-04-10 11:46:24 +0200

    movie module update

---
+++ cinema-frontend/src/app/movie/components/edit-movie-form/edit-movie-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-movie-form',
+  templateUrl: './edit-movie-form.component.html',
+  styleUrls: ['./edit-movie-form.component.scss']
+})
+export class EditMovieFormComponent {
+
+}
--- cinema-frontend/src/app/movie/movie.module.ts
+++ cinema-frontend/src/app/movie/movie.module.ts
@@ -1,15 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CreateMovieFormComponent } from './create-movie-form/create-movie-form.component';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {CreateMovieFormComponent} from "./components/create-movie-form/create-movie-form.component";
+import {MovieDetailsComponent} from "./components/movie-details/movie-details.component";
+import {MovieListComponent} from "./components/movie-list/movie-list.component";
+import {EditMovieFormComponent} from "./components/edit-movie-form/edit-movie-form.component";
 
 
 @NgModule({
   declarations: [
-    CreateMovieFormComponent
+    CreateMovieFormComponent,
+    MovieDetailsComponent,
+    MovieListComponent,
+    EditMovieFormComponent
   ],
   imports: [
     CommonModule
   ]
 })
-export class MovieModule { }
+export class MovieModule {
+}
