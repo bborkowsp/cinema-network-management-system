cff3bc8c0dd622158b2aaf847eef00142a7e0931
bborkowsp <pawel.borkowski0704@gmail.com>
2024-05-29 17:42:43 +0200

    showing file name in about cinema

--- cinema-frontend/src/app/cinema/components/cinema-form/components/steps-container/components/about-cinema/about-cinema.component.ts
+++ cinema-frontend/src/app/cinema/components/cinema-form/components/steps-container/components/about-cinema/about-cinema.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormControl, FormGroup, FormGroupDirective, NgForm} from "@angular/forms";
 
 @Component({
@@ -6,11 +6,10 @@ import {FormControl, FormGroup, FormGroupDirective, NgForm} from "@angular/forms
   templateUrl: './about-cinema.component.html',
   styleUrls: ['./about-cinema.component.scss']
 })
-export class AboutCinemaComponent {
+export class AboutCinemaComponent implements OnInit {
   @Input({required: true}) form!: FormGroupDirective | NgForm;
   @Input({required: true}) formGroup!: FormGroup;
   selectedFileName: string | null = null;
-
 
   get nameControl(): FormControl {
     return this.formGroup.get('aboutCinema')?.get('name') as FormControl;
@@ -23,6 +22,22 @@ export class AboutCinemaComponent {
   get imageControl(): FormControl {
     return this.formGroup.get('aboutCinema')?.get('image') as FormControl;
   }
+
+  ngOnInit(): void {
+    const file = this.imageControl.value;
+    if (file) {
+      this.selectedFileName = file.name;
+    }
+
+    this.imageControl.valueChanges.subscribe((value: File) => {
+      if (value) {
+        this.selectedFileName = value.name;
+      } else {
+        this.selectedFileName = null;
+      }
+    });
+  }
+
 
   onFileSelected(event: any) {
     const file: File = event.target.files[0];
