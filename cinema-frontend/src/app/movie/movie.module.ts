604e5e789277caf97d9a87e1d21747fe9db0861c
bborkowsp <pawel.borkowski0704@gmail.com>
2024-05-30 13:51:33 +0200

    deleting old forms

--- cinema-frontend/src/app/movie/movie.module.ts
+++ cinema-frontend/src/app/movie/movie.module.ts
@@ -2,47 +2,43 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MovieDetailsComponent} from "./components/movie-details/movie-details.component";
 import {MovieListComponent} from "./components/movie-list/movie-list.component";
-import {EditMovieFormComponent} from "./components/edit-movie-form/edit-movie-form.component";
 import {MatPaginatorModule} from "@angular/material/paginator";
 import {MatTableModule} from "@angular/material/table";
 import {SharedModule} from "../shared/shared.module";
 import {MatDividerModule} from "@angular/material/divider";
-import {CreateMovieComponent} from "./components/create-movie/create-movie.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import {MatButtonModule} from "@angular/material/button";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MovieFormComponent} from './components/movie-form/movie-form.component';
 import {
   CreateTitleFormFrameComponentComponent
-} from './components/create-movie/components/create-title-form-frame-component/create-title-form-frame-component.component';
+} from "./components/movie-form/components/create-title-form-frame-component/create-title-form-frame-component.component";
 import {
   InformationFormFrameComponentComponent
-} from './components/create-movie/components/information-form-frame-component/information-form-frame-component.component';
-import {MatDatepickerModule} from "@angular/material/datepicker";
+} from "./components/movie-form/components/information-form-frame-component/information-form-frame-component.component";
 import {
   ProductionDetailsFormFrameComponentComponent
-} from './components/create-movie/components/production-details-form-frame-component/production-details-form-frame-component.component';
+} from "./components/movie-form/components/production-details-form-frame-component/production-details-form-frame-component.component";
 import {
   ImageAndTrailerFormFrameComponentComponent
-} from './components/create-movie/components/image-and-trailer-form-frame-component/image-and-trailer-form-frame-component.component';
-import {MatIconModule} from "@angular/material/icon";
-import {
-  AgeRestrictionAndGenresFormFrameComponentComponent
-} from './components/create-movie/components/age-restriction-and-genres-form-frame-component/age-restriction-and-genres-form-frame-component.component';
-import {MatSelectModule} from "@angular/material/select";
+} from "./components/movie-form/components/image-and-trailer-form-frame-component/image-and-trailer-form-frame-component.component";
 import {
   ProjectionDetailsFormFrameComponentComponent
-} from './components/create-movie/components/projection-details-form-frame-component/projection-details-form-frame-component.component';
-import {MatNativeDateModule} from "@angular/material/core";
-import { MovieFormComponent } from './components/movie-form/movie-form.component';
+} from "./components/movie-form/components/projection-details-form-frame-component/projection-details-form-frame-component.component";
+import {
+  AgeRestrictionAndGenresFormFrameComponentComponent
+} from "./components/movie-form/components/age-restriction-and-genres-form-frame-component/age-restriction-and-genres-form-frame-component.component";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSelectModule} from "@angular/material/select";
 
 
 @NgModule({
   declarations: [
     MovieDetailsComponent,
-    CreateMovieComponent,
     MovieListComponent,
-    EditMovieFormComponent,
     CreateTitleFormFrameComponentComponent,
     InformationFormFrameComponentComponent,
     ProductionDetailsFormFrameComponentComponent,
