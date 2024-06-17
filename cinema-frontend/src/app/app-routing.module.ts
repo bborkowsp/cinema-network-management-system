7fb2f0bf0b94ae744e6f8f735a66b3ebb57dabfa
bborkowsp <pawel.borkowski0704@gmail.com>
2024-06-17 11:31:39 +0200

    adding addmin role to routing

--- cinema-frontend/src/app/app-routing.module.ts
+++ cinema-frontend/src/app/app-routing.module.ts
@@ -32,95 +32,95 @@ const routes: Routes = [
     path: 'cinemas',
     component: CinemaTableComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.CINEMA_NETWORK_MANAGER]}
+    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.ADMIN]}
   },
   {
     path: 'cinemas/create',
     component: CinemaFormComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.CINEMA_NETWORK_MANAGER]}
+    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.ADMIN]}
   },
   {
     path: 'cinemas/edit/:name',
     component: CinemaFormComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.CINEMA_NETWORK_MANAGER]}
+    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.ADMIN]}
   },
   {
     path: 'cinemas/details/:name',
     component: CinemaDetailsComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.CINEMA_NETWORK_MANAGER]}
+    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.ADMIN]}
   },
   {
     path: 'projection-technologies',
     component: ProjectionTechnologyTableComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.CINEMA_MANAGER]}
+    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.CINEMA_MANAGER, Role.ADMIN]}
   },
   {
     path: 'projection-technologies/create',
     component: ProjectionTechnologyFormComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.CINEMA_NETWORK_MANAGER]}
+    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.ADMIN]}
   },
   {
     path: 'projection-technologies/edit/:technology',
     component: ProjectionTechnologyFormComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.CINEMA_NETWORK_MANAGER]}
+    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.ADMIN]}
   },
   {
     path: 'projection-technologies/details/:technology',
     component: ProjectionTechnologyDetailsComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.CINEMA_MANAGER]}
+    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.CINEMA_MANAGER, Role.ADMIN]}
   },
   {
     path: 'movies',
     component: MovieTableComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.CINEMA_MANAGER]}
+    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.CINEMA_MANAGER, Role.ADMIN]}
   },
   {
     path: 'movies/create',
     component: MovieFormComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.CINEMA_NETWORK_MANAGER]}
+    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.ADMIN]}
   },
   {
     path: 'movies/edit/:title',
     component: MovieFormComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.CINEMA_NETWORK_MANAGER]}
+    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.ADMIN]}
   },
   {
     path: 'movies/details/:title',
     component: MovieDetailsComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.CINEMA_MANAGER]}
+    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.CINEMA_MANAGER, Role.ADMIN]}
   },
   {
     path: 'home', component: HomeComponent, canActivate: [AuthGuard],
-    data: {roles: [Role.CINEMA_MANAGER, Role.CINEMA_NETWORK_MANAGER]}
+    data: {roles: [Role.CINEMA_MANAGER, Role.CINEMA_NETWORK_MANAGER, Role.ADMIN]}
   },
   {
     path: 'cinema-managers',
     component: CinemaManagerTableComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.CINEMA_NETWORK_MANAGER]}
+    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.ADMIN]}
   },
   {
     path: 'cinema-managers/create',
     component: CinemaManagerFormComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.CINEMA_NETWORK_MANAGER]}
+    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.ADMIN]}
   },
   {
     path: 'cinema-managers/edit/:email',
     component: CinemaManagerFormComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.CINEMA_NETWORK_MANAGER]}
+    data: {roles: [Role.CINEMA_NETWORK_MANAGER, Role.ADMIN]}
   },
   {
     path: 'repertory',
