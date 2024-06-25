7c1bf354a85ca5e06705ff6cb3bb8f3870351861
bborkowsp <pawel.borkowski0704@gmail.com>
2024-06-25 12:43:22 +0200

    chaning project name of frontend app

--- cinema-admin-frontend/src/app/app.component.spec.ts
+++ cinema-admin-frontend/src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cinema-frontend'`, () => {
+  it(`should have as title 'cinema-admin-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cinema-frontend');
+    expect(app.title).toEqual('cinema-admin-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cinema-frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cinema-admin-frontend app is running!');
   });
 });
