fbf6de38d0914a83094ea47217e3e3b269f23879
bborkowsp <pawel.borkowski0704@gmail.com>
2024-08-20 20:25:39 +0200

    update

--- cinema-customer-frontend/src/app/repertory/components/buy-ticket/components/steps/seat-selection/seat-selection.component.ts
+++ cinema-customer-frontend/src/app/repertory/components/buy-ticket/components/steps/seat-selection/seat-selection.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {Router} from "@angular/router";
 import {ScreeningResponse} from "../../../../../dtos/response/screening.response";
 import {SeatResponse} from "../../../../../dtos/response/seat.response";
+import "../../../../../../_shared/styles/_colors.scss";
 
 @Component({
   selector: 'app-seat-selection',
@@ -21,7 +22,28 @@ export class SeatSelectionComponent {
   }
 
   selectSeat(seat: SeatResponse) {
+    if (this.selectedSeats.includes(seat)) {
+      this.selectedSeats = this.selectedSeats.filter(s => s !== seat);
+      return;
+    }
     this.selectedSeats.push(seat);
+  }
 
+  getSeatBackgroundColor(seat: SeatResponse): string {
+    if (this.selectedSeats.includes(seat)) {
+      switch (seat.seatZone) {
+        case 'STANDARD':
+          return '#c28dff';
+        case 'VIP':
+          return '#ffd24d';
+        case 'PROMO':
+          return '#66ff66';
+        case 'WHEELCHAIR':
+          return '#97d3f1';
+        default:
+          return 'white';
+      }
+    }
+    return 'white';
   }
 }
