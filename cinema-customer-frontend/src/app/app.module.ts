03771a39f83f7807ce6661cf4ce0f1cb2a7d14bc
bborkowsp <pawel.borkowski0704@gmail.com>
2024-09-06 11:43:43 +0200

    @angular/core@18 @angular/cli@18

--- cinema-customer-frontend/src/app/app.module.ts
+++ cinema-customer-frontend/src/app/app.module.ts
@@ -6,29 +6,23 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {SharedModule} from "./_shared/shared.module";
 import {RepertoryModule} from "./repertory/repertory.module";
 import {AppRoutingModule} from "./app-routing.module";
-import {HttpClientModule} from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import {DatePipe} from "@angular/common";
 import {MatDialogModule} from "@angular/material/dialog";
 import {UserModule} from "./user/user.module";
 
-@NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    SharedModule,
-    RepertoryModule,
-    HttpClientModule,
-    MatDialogModule,
-    UserModule
-  ],
-  providers: [
-    DatePipe
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        SharedModule,
+        RepertoryModule,
+        MatDialogModule,
+        UserModule], providers: [
+        DatePipe,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule {
 }
--- cinema-customer-frontend/src/app/auth/service/auth.service.ts
+++ cinema-customer-frontend/src/app/auth/service/auth.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import {Router} from "@angular/router";
 import {BehaviorSubject} from "rxjs";
 import * as moment from "moment";
