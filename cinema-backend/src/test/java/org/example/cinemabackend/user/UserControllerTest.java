d6799de30a03776c8c12abb5fc3aec280d40f74f
bborkowsp <pawel.borkowski0704@gmail.com>
2024-09-13 11:07:29 +0200

    User controller test

--- cinema-backend/src/test/java/org/example/cinemabackend/user/UserControllerTest.java
+++ cinema-backend/src/test/java/org/example/cinemabackend/user/UserControllerTest.java
@@ -2,6 +2,7 @@ package org.example.cinemabackend.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.cinemabackend.user.application.dto.request.CreateUserRequest;
+import org.example.cinemabackend.user.application.dto.request.UpdateUserRequest;
 import org.example.cinemabackend.user.core.domain.Role;
 import org.example.cinemabackend.user.core.domain.User;
 import org.example.cinemabackend.user.core.port.secondary.UserRepository;
@@ -25,8 +26,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -110,5 +110,86 @@ public class UserControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(createUserRequest)))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Order(5)
+    @Test
+    void givenUsersInDb_whenGetUsers_thenPageWithUsersIsReturned() throws Exception {
+        //When, Then
+        mockMvc.perform(get(USERS_ENDPOINT_URL))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON),
+                        jsonPath("$.content").isNotEmpty(),
+                        jsonPath("$.content").isArray(),
+                        jsonPath("$.content[0].email").value(EMAIL + Role.CINEMA_MANAGER),
+                        jsonPath("$.content[0].firstName").value(FIRST_NAME),
+                        jsonPath("$.content[3].email").value(EMAIL + Role.ADMIN),
+                        jsonPath("$.content[3].firstName").value(FIRST_NAME)
+                );
+    }
+
+    @Order(6)
+    @Test
+    void givenUsersInDb_whenGetUser_thenUserIsReturned() throws Exception {
+        //Given
+        final var email = EMAIL + Role.CINEMA_MANAGER;
+
+        //When, Then
+        final var url = USERS_ENDPOINT_URL + "/" + email;
+        mockMvc.perform(get(url))
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON),
+                        jsonPath("$.email").value(email),
+                        jsonPath("$.role").value(Role.CINEMA_MANAGER.toString()),
+                        jsonPath("$.firstName").value(FIRST_NAME),
+                        jsonPath("$.lastName").value(LAST_NAME)
+                );
+    }
+
+    @Order(7)
+    @Test
+    void givenUsersInDb_whenUpdateUser_thenUserIsUpdated() throws Exception {
+        //Given
+        final var email = EMAIL + Role.CINEMA_MANAGER;
+        final var updateEmail = "new" + email;
+        final var updateFirstName = "Jane";
+        final var updateLastName = "Eod";
+        final var updatePassword = "new" + PASSWORD;
+        final var updateUserRequest = new UpdateUserRequest(updateFirstName, updateLastName, updateEmail, PASSWORD, updatePassword);
+
+        //When
+        final var url = CINEMA_NETWORK_MANAGERS_ENDPOINT_PREFIX + "/" + email;
+        mockMvc.perform(patch(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateUserRequest)))
+                .andExpect(status().isNoContent());
+
+        //Then
+        Optional<User> optionalUser = userRepository.findByEmail(updateEmail);
+        assertThat(optionalUser).isPresent();
+        User user = optionalUser.get();
+        assertAll(
+                () -> assertEquals(updateUserRequest.firstName(), user.getFirstName()),
+                () -> assertEquals(updateUserRequest.lastName(), user.getLastName()),
+                () -> assertEquals(updateUserRequest.email(), user.getEmail())
+        );
+    }
+
+
+    @Order(8)
+    @Test
+    void givenUsersInDb_whenDeleteUser_thenUserIsDeleted() throws Exception {
+        //Given
+        final var email = EMAIL + Role.CINEMA_NETWORK_MANAGER;
+
+        //When
+        final var url = USERS_ENDPOINT_URL + "/" + email;
+        mockMvc.perform(delete(url))
+                .andExpect(status().isNoContent());
+
+        mockMvc.perform(get(url))
+                .andExpect(status().isNotFound());
     }
 }
