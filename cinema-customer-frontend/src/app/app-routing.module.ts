a2546de22cd3c54bd7da4feb90ce9bede3edda35
bborkowsp <pawel.borkowski0704@gmail.com>
2024-09-14 11:36:40 +0200

    screening list update

--- cinema-customer-frontend/src/app/app-routing.module.ts
+++ cinema-customer-frontend/src/app/app-routing.module.ts
@@ -23,7 +23,6 @@ import {
 
 const routes: Routes = [
   {path: '', component: RepertoryComponent},
-  {path: '**', component: RepertoryComponent},
   {path: 'repertory', component: RepertoryComponent},
   {path: 'buy-ticket/:id', component: BuyTicketComponent},
   {path: 'login', component: LoginComponent},
